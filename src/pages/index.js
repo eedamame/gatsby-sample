aa9a310c34ce4393c333c65d1151b95493f62ba2
eedamame <eedamame@gmail.com>
2021-05-18 21:19:48 +0900

    一通りの設定と画像追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,16 @@
 import * as React from "react"
+import Logo from "../images/logo.svg"
+import Hero from "../images/hero.jpg"
+import Wave from "../images/wave.svg"
+import Fruit from "../images/fruit.jpg"
+import Grain from "../images/grain.jpg"
+import Beverage from "../images/beverage.jpg"
+import Berry from "../images/berry.jpg"
+import LogoW from "../images/logo-w.svg"
+
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
 }
 
 // data
@@ -129,54 +64,109 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
+      <header class="header">
+        <div class="container">
+          <div class="site">
+            <a href="base-index.html">
+              <img src={Logo} alt="ESSENTIALS" />
+            </a>
+          </div>
+          <nav class="nav">
+            <ul>
+              <li><a href="base-index.html">TOP</a></li>
+              <li><a href="base-about.html">ABOUT</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <section class="hero">
+        <figure>
+          <img src={Hero} alt="" />
+        </figure>
+        <div class="catch">
+          <h1>There is no love sincerer than<br /> the love of food.</h1>
+          <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
+        </div>
+        <div class="wave">
+          <img src={Wave} alt="" />
+        </div>
+      </section>
+
+
+      <section class="food">
+        <div class="container">
+          <h2 class="bar">Food <span>Essence</span></h2>
+
+          <div class="details">
+            <div class="detail">
+              <figure>
+                <img src={Fruit} alt="" />
+              </figure>
+              <h3>フルーツ</h3>
+              <p>FRUIT</p>
+                <p>甘くてすっぱくておいしい果実たち。<br />旬のフルーツを満喫します。</p>
+            </div>
+
+            <div class="detail">
+              <figure>
+                <img src={Grain} alt="" />
+              </figure>
+              <h3>穀物</h3>
+              <p>GRAIN</p>
+                <p>食事の基本となる穀物。<br />毎日の活動のエネルギー源になります。</p>
+            </div>
+
+            <div class="detail">
+              <figure>
+                <img src={Beverage} alt="" />
+              </figure>
+              <h3>飲み物</h3>
+              <p>BEVERAGE</p>
+                <p>リラックスするのに欠かせない飲み物。<br />お気に入りの一杯はありますか？</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="photo">
+        <h2 class="sr-only">Photo</h2>
+        <figure>
+          <img src={Berry} alt="赤く熟したベリー" />
+        </figure>
+      </section>
+
+      <footer class="footer">
+        <div class="container">
+          <div class="site">
+          <a href="base-index.html">
+            <img src={LogoW} alt="ESSENTIALS" />
+            <p>おいしい食材と食事を探求するサイト</p>
           </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
+          </div>
+
+          <ul class="sns">
+            <li>
+              <a href="https://twitter.com/">
+                <i class="fab fa-twitter"></i>
+                <span class="sr-only">Twitter</span>
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+            </li>
+            <li>
+              <a href="https://facebook.com/">
+                <i class="fab fa-facebook-square"></i>
+                <span class="sr-only">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://instagram.com/">
+                <i class="fab fa-instagram"></i>
+                <span class="sr-only">Instagram</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </main>
   )
 }
